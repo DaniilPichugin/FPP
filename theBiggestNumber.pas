0d4d4acff191b1a6f5edf6a1a08b9175222fcc03
Daniil Pichugin <38327863+DaniilPichugin@users.noreply.github.com>
2018-10-11 23:31:25 +0300

    Add files via upload

---
+++ theBiggestNumber.pas
@@ -0,0 +1,20 @@
+program theBiggestNumber;
+var
+  a,b,c: integer;
+begin
+  writeln('Укажите числа так чтобы они не были равными!');
+  {часть ввода данных: начало}
+  writeln('Введите первое число: '); readln(a);
+  writeln('Введите второе число: '); readln(b);
+  writeln('Введите третье число: '); readln(c);
+  {часть ввода данных: конец}
+
+  {часть сравнения переменных: }
+  if((a>b)and(a>c))then
+    writeln('Первое число - самое большое!');
+  if((b>a)and(b>c))then
+    writeln('Второе число - самое большое!');
+  if((c>a)and(c>b))then
+    writeln('Третье число - самое большое!');
+  {if((a=b) or (a=c) or )}{доработать}
+end.
